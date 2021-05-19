39c4e106b0b38de21142ce44fc12bf1f57cfea53
lxcao <caolingxin@hotmail.com>
2021-05-19 08:31:09 +0800

    splashpage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+/*
+ * @Author: clingxin
+ * @Date: 2021-05-19 08:27:14
+ * @LastEditors: clingxin
+ * @LastEditTime: 2021-05-19 08:30:26
+ * @FilePath: /flutter_object_detection/lib/pages/home_page.dart
+ */
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -2,10 +2,11 @@
  * @Author: clingxin
  * @Date: 2021-05-19 07:54:47
  * @LastEditors: clingxin
- * @LastEditTime: 2021-05-19 08:04:23
+ * @LastEditTime: 2021-05-19 08:30:46
  * @FilePath: /flutter_object_detection/lib/pages/splash_page.dart
  */
 import 'package:flutter/material.dart';
+import 'package:flutter_object_detection/pages/home_page.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 class SplashPage extends StatefulWidget {
@@ -17,7 +18,8 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
-      seconds: 12,
+      seconds: 6,
+      navigateAfterSeconds: HomePage(),
       useLoader: true,
       loaderColor: Theme.of(context).primaryColor,
       loadingText: Text(
